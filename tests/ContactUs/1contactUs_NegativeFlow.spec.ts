480c3aa6b562e7ed166f10e9e5ea855b3bd164a7
unknown <oleks.automation@gmail.com>
2026-08-19 21:17:31 +0300

    finished negative flow

--- tests/ContactUs/1contactUs_NegativeFlow.spec.ts
+++ tests/ContactUs/1contactUs_NegativeFlow.spec.ts
@@ -1,27 +1,59 @@
 import { test, expect } from "@playwright/test"
 
 
-test("Verify CONTACT US page", async ({ page, context }) => {
+test("Verify CONTACT US page, negtive flow", async ({ page, context }) => {
 
     await page.goto('/index.html');
     const pagePromise = context.waitForEvent('page');
     await page.locator('#contact-us').click();
     const contactPage = await pagePromise;
     await contactPage.waitForLoadState('networkidle');
-    
+
     const pageFields = [
-        { selector: contactPage.getByPlaceholder("First Name"), value: "John"}
-        { selector: contactPage.getByPlaceholder("Last Name"), value: "Doe"}
-        { selector: contactPage.getByPlaceholder("Email Address"), value: "test@gmail.com"}
-        { selector: contactPage.getByPlaceholder("Comments"), value: "This is a test comment"}
+        { selector: contactPage.getByPlaceholder("First Name"), value: "John" },
+        { selector: contactPage.getByPlaceholder("Last Name"), value: "Doe" },
+        { selector: contactPage.getByPlaceholder("Email Address"), value: "test@gmail.com" },
+        { selector: contactPage.getByPlaceholder("Comments"), value: "This is a test comment" }
     ];
 
+    for (let skipIndex = 0; skipIndex < pageFields.length; skipIndex++) {
 
-    await contactPage.getByPlaceholder("First Name").fill("John");
-    await contactPage.getByPlaceholder("Last Name").fill("Doe");
-    await contactPage.getByPlaceholder("Email Address").fill("test@gmail.com");
-    await contactPage.getByPlaceholder("Comments").fill("This is a test comment.");
+        for (let i = 0; i < pageFields.length; i++) {
+            if (i === skipIndex) continue;
+            await pageFields[i].selector.fill(pageFields[i].value);
+        }
+        await contactPage.getByRole("button", { name: "SUBMIT" }).click();
+
+        if (skipIndex === 2) {
+            await expect(contactPage.locator('text="Error: all fields are required"')).toBeVisible();
+            await expect(contactPage.locator('text="Error: Invalid email address"')).toBeVisible();
+
+        } else {
+            await expect(contactPage.locator('text="Error: all fields are required"')).toBeVisible();
+        };
+        await contactPage.locator(".eb.eb-retry").click();
+        console.log("Iteration #" + skipIndex + " is completed");
+    }
+
+
+    for (let i = 0; i < pageFields.length; i++) {
+        await pageFields[i].selector.fill("");
+    }
     await contactPage.getByRole("button", { name: "SUBMIT" }).click();
-    await expect(contactPage.locator('text="Thank You for your Message!"')).toBeVisible();
+    await expect(contactPage.locator('text="Error: all fields are required"')).toBeVisible();
+    await expect(contactPage.locator('text="Error: Invalid email address"')).toBeVisible();
+    await contactPage.locator(".eb.eb-home").click();
+    await expect(contactPage).toHaveURL('https://webdriveruniversity.com/index.html');
 
 })
+
+//Error: Invalid email address
+
+// await contactPage.getByPlaceholder("First Name").fill("John");
+// await contactPage.getByPlaceholder("Last Name").fill("Doe");
+// await contactPage.getByPlaceholder("Email Address").fill("test@gmail.com");
+// await contactPage.getByPlaceholder("Comments").fill("This is a test comment.");
+// await contactPage.getByRole("button", { name: "SUBMIT" }).click();
+// await expect(contactPage.locator('text="Thank You for your Message!"')).toBeVisible();
+
+
